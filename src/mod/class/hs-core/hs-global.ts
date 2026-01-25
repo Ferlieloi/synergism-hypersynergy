f7ebdd6a338f53ffa0b26e3496e5bcec19fa61be
Ferren Verhoeven <61938890+Ferlieloi@users.noreply.github.com>
2026-01-25 23:55:53 +0100

    2.10
    -Heater data fixed!!!

--- src/mod/class/hs-core/hs-global.ts
+++ src/mod/class/hs-core/hs-global.ts
@@ -29,7 +29,7 @@ export const HSGlobal: IHSGlobal = class {
     // --- GENERAL ---
 
     static General = {
-        currentModVersion: '2.9.7b',
+        currentModVersion: '2.10.0',
         isLatestVersion: true,
 
         modGithubUrl: 'https://github.com/ahvonenj/synergism-hypersynergy/',
