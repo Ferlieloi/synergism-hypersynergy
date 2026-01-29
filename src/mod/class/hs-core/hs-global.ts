47a24564a150df1469ee6486bdb357d02a680779
maenhiir <115376767+maenhiir@users.noreply.github.com>
2026-01-29 13:12:16 +0100

    fix: some texts a bit more clear + capital letters

--- src/mod/class/hs-core/hs-global.ts
+++ src/mod/class/hs-core/hs-global.ts
@@ -29,7 +29,7 @@ export const HSGlobal: IHSGlobal = class {
     // --- GENERAL ---
 
     static General = {
-        currentModVersion: '2.10.0d',
+        currentModVersion: '2.10.0e',
         isLatestVersion: true,
 
         modGithubUrl: 'https://github.com/ahvonenj/synergism-hypersynergy/',
