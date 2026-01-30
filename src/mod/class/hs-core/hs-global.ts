39f00535290899b5197aa6d790e67acadfc0deff
Ferren Verhoeven <61938890+Ferlieloi@users.noreply.github.com>
2026-01-30 18:31:16 +0100

    2.10.1a
    -french fix

--- src/mod/class/hs-core/hs-global.ts
+++ src/mod/class/hs-core/hs-global.ts
@@ -29,7 +29,7 @@ export const HSGlobal: IHSGlobal = class {
     // --- GENERAL ---
 
     static General = {
-        currentModVersion: '2.10.1',
+        currentModVersion: '2.10.1a',
         isLatestVersion: true,
 
         modGithubUrl: 'https://github.com/ahvonenj/synergism-hypersynergy/',
