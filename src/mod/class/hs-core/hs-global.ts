90a0d154b4f9aa2a39ddae7d6084ea14c7fa346e
Ferren Verhoeven <61938890+Ferlieloi@users.noreply.github.com>
2026-02-13 00:10:32 +0100

    2.10.5b
    Added a new "Cycle BUY" button, instead of replacing the vanilla BUY ALL button.
    Added a setting to enable or disable it
    Fixed rates/s, to be a true overall rate/s (since start)
    When Advanced Data Collection is disabled: we don't record the full arrays of data anymore, which should be a lot better for memory during long autosing sessions
    When Advanced Data Collection is enabled:
    --- The data are now stored in IndexedDB instead of localStorage, which should be better...?
    --- Use lazy compression (batch compression every 10 singularities) to avoid blocking fast loops, using LZ-string for ~55% storage savings in IndexedDB
    --- Add lz-string dependency for compression (we can remove the compression if you don't like it)
    Some small changes or optimizations here and there

--- src/mod/class/hs-core/hs-global.ts
+++ src/mod/class/hs-core/hs-global.ts
@@ -29,7 +29,7 @@ export const HSGlobal: IHSGlobal = class {
     // --- GENERAL ---
 
     static General = {
-        currentModVersion: '2.10.5yop',
+        currentModVersion: '2.10.5b',
         isLatestVersion: true,
 
         modGithubUrl: 'https://github.com/ahvonenj/synergism-hypersynergy/',
