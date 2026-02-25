88732b7bc18a7c7c6b7fb8671dfec2c6d08f02be
maenhiir <115376767+maenhiir@users.noreply.github.com>
2026-02-25 01:26:12 +0100

    add my own loaders to match latest

---
+++ src/loader/tampermonkey-bridge.user.shewchou.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         HyperSynergism Bridge Loader (Shewchou)
+// @namespace    https://github.com/Ferlieloi
+// @version      1.0
+// @description  Evergreen bridge to official Hypersynergism mod
+// @author       Ferlieloi
+// @match        https://synergism.cc/*
+// @require      https://cdn.jsdelivr.net/gh/maenhiir/synergism-hypersynergy@latest/src/loader/hypersynergism.user.shewchou.js
+// @grant        none
+// @run-at       document-start
+// @updateURL    https://cdn.jsdelivr.net/gh/maenhiir/synergism-hypersynergy@latest/src/loader/tampermonkey-bridge.user.shewchou.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/maenhiir/synergism-hypersynergy@latest/src/loader/tampermonkey-bridge.user.shewchou.js
+// ==/UserScript==
+
+// Production Evergreen Bridge.
+// Install once - auto-updates.
+// Loads the full "true" loader (hypersynergism.user.js), which handles game patching and mod loading.
